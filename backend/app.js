59c8f3a11ef0ec91115d1047b0cc3bb15effa2a0
JayaVardhan2039 <gjvardhan992003@gmail.com>
2025-02-07 15:28:08 +0530

    user registration done

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,21 @@
 const dotenv=require('dotenv');
 dotenv.config();
-
+const connectToDb=require('./db/db');
+connectToDb();
 const express=require('express');
 const app=express();
 
 const cors=require('cors');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
+
+const userRoutes=require('./routes/user.routes');
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+
+app.use('/users',userRoutes);
 
 module.exports=app;
