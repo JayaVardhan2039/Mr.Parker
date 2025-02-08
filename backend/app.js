b99c2266b9d45f09da53e381a51e7b232f833820
JayaVardhan2039 <gjvardhan992003@gmail.com>
2025-02-08 14:24:40 +0530

    user logout done

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,12 @@ const connectToDb=require('./db/db');
 connectToDb();
 const express=require('express');
 const app=express();
-
+const cookieParser=require('cookie-parser');
+const userRoutes=require('./routes/user.routes');
 const cors=require('cors');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-
-const userRoutes=require('./routes/user.routes');
+app.use(cookieParser());
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send('Hello World');
